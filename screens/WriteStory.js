a0a75e5a671e8992e7b7ab840884cb75e801f179
Mayank <rameshratre@Rameshs-MacBook-Air.local>
2021-01-26 22:21:26 +0530

    qvqer

--- screens/WriteStory.js
+++ screens/WriteStory.js
@@ -1,16 +1,6 @@
 import React from 'react'
-import { 
-  ImageBackground, 
-  View, 
-  Text, 
-  StyleSheet,
-  TouchableOpacity,
-  KeyboardAvoidingView,
-  Platform,
-  ToastAndroid,
-} from 'react-native'
+import { ImageBackground, View, Text, StyleSheet, TouchableOpacity, KeyboardAvoidingView, ToastAndroid} from 'react-native'
 import { TextInput } from 'react-native-paper';
-import firebase from 'firebase';
 import db from '../config';
 
 const image = { uri: "https://wallpapercave.com/wp/wp2297884.jpg" };
@@ -19,13 +9,10 @@ const image = { uri: "https://wallpapercave.com/wp/wp2297884.jpg" };
 export default class WriteStory extends React.Component{
   constructor(props) {
     super(props);
-    this.handleTitle = this.handleTitle.bind(this);
-    this.handleAuthor = this.handleAuthor.bind(this);
-    this.handleContent = this.handleContent.bind(this);
     this.state = {
       title: '',
       author: '',
-      content: ''
+      content: '',
     };
   }
 
@@ -37,73 +24,69 @@ export default class WriteStory extends React.Component{
   handleAuthor(event) {
     this.setState({author: event.target.value});
     console.log("title is author");
-
   }
 
   handleContent(event) {
     this.setState({content: event.target.value});
     console.log("title is content");
-
   }
 
   submitStory = async ()=>{
     //add a story
     db.collection("writestory").add({
-      'title' : this.state.title,
-      'author' : this.state.author,
-      'content' : this.state.content,
-      'date'   : firebase.firestore.Timestamp.now().toDate()
+      title : this.state.title,
+      author : this.state.author,
+      content : this.state.content,
     })
     this.setState({
       title: '',
       author: '',
       content: ''
     })
-  }
-
-  showAlert(){
-    alert("YOUR STORY IS SUBMITTED :)");
+    // ToastAndroid.show('Your story has been sumitted', ToastAndroid.SHORT)
   }
 
     render(){
-
-      const showToast = () => {
-        ToastAndroid.show("A pikachu appeared nearby !", ToastAndroid.SHORT);
-      };
-
       return(
         <View style={styles.container}>
           <ImageBackground source={image} style={styles.image}>
-            <KeyboardAvoidingView
-              behavior={Platform.OS === "ios" ? "padding" : "height"}
-              style={styles.container}
-            >
+            <KeyboardAvoidingView behavior="padding" style={styles.container}>
               <Text style={styles.headers}>WRITE STORY</Text>
-              <TextInput
-                label="STORY TITLE"
+              <TextInput 
+                placeholder="Story Title"
+                onChangeText= {(text)=>{
+                    this.setState({
+                        title: text
+                    })
+                }}
+                placeholderTextColor='black'
                 value={this.state.title}
-                onChange={this.handleTitle} 
                 style={styles.textInput}
               />
               <TextInput
-                label="AUTHOR"
+                placeholder="Author"
+                onChangeText= {(text)=>{
+                    this.setState({
+                        author: text
+                    })
+                }}
+                placeholderTextColor='black'
                 value={this.state.author}
-                onChange={this.handleAuthor} 
-                style={styles.textInput}
-              /> 
-              <TextInput
-                label="WRITE YOUR STORY"
-                value={this.state.content}
-                onChange={this.handleContent} 
+                style={styles.textInput} 
+              />
+              <TextInput 
+                placeholder="Write your story"
+                onChangeText= {(text)=>{
+                    this.setState({
+                        storyText: text
+                    })
+                }}
+                placeholderTextColor='black'
+                value={this.state.storyText}
                 style={styles.textInput}
                 multiline={true}
-              />         
-              <TouchableOpacity onPress={() => 
-                { 
-                  //this.submitStory(); 
-                  this.showAlert();
-                }
-                }>
+              />       
+              <TouchableOpacity onPress={() => {this.submitStory();}}>
                 <Text style={styles.buttonStyle}>SUBMIT</Text>
               </TouchableOpacity>
             </KeyboardAvoidingView>
@@ -112,6 +95,8 @@ export default class WriteStory extends React.Component{
       )
     }
   }
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -125,32 +110,32 @@ const styles = StyleSheet.create({
   },
   textInput:{
     width: 500,
-    marginLeft: '35%',
-    marginBottom: '4%',
+    marginLeft: "35%",
+    marginBottom: "4%",
   },
   headers:{
-    fontWeight: 'bold',
-    margin: '50px',
-    fontSize: '40px',
-    fontFamily: 'Helvetica',
-    border: '2px solid black',
-    padding:'30px',
-    textAlign: 'center',
-    width: '500px',
-    marginLeft: '35%',
-    borderRadius: '20px',
-    color: 'orange',
+    fontWeight: "bold",
+    margin: "50px",
+    fontSize: "40px",
+    fontFamily: "Helvetica",
+    border: "2px solid black",
+    padding: "30px",
+    textAlign: "center",
+    width: "500px",
+    marginLeft: "35%",
+    borderRadius: "20px",
+    color: "orange",
   },
   buttonStyle:{
-    backgroundColor: 'orange',
+    backgroundColor: "orange",
     padding: 10,
     margin: 10,
     width: 120,
     height: 50,
-    borderRadius: '20px',
-    textAlign: 'center',
-    justifyContent: 'center',
-    marginLeft: '45%',
-    fontSize: '25px',
+    borderRadius: "20px",
+    textAlign: "center",
+    justifyContent: "center",
+    marginLeft: "45%",
+    fontSize: "25px",
   },
 });
